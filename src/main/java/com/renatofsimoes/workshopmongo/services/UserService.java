ff9a7a396d8806c392fa0f14779bf8742d233c1e
Renato Fioroni Simoes <renatofsim@gmail.com>
2025-03-11 13:01:58 -0300

    User update with PUT

--- src/main/java/com/renatofsimoes/workshopmongo/services/UserService.java
+++ src/main/java/com/renatofsimoes/workshopmongo/services/UserService.java
@@ -20,21 +20,33 @@ public class UserService {
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
-	public User findById(String id) { 
-		 Optional<User> obj = userRepository.findById(id); 
-		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
-	public void delete (String id) {
-		findById(id); //caso não encontrar o id dado já lançara a exceção
+
+	public void delete(String id) {
+		findById(id); // caso não encontrar o id dado já lançará a exceção
 		userRepository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
