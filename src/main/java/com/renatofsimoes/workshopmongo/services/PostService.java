4e633f29d573238a1327767cd643152db352d996
Renato Fioroni Simoes <renatofsim@gmail.com>
2025-03-12 10:35:54 -0300

    Getting a post by id

---
+++ src/main/java/com/renatofsimoes/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.renatofsimoes.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renatofsimoes.workshopmongo.domain.Post;
+import com.renatofsimoes.workshopmongo.repository.PostRepository;
+import com.renatofsimoes.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
